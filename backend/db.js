eefab64f1d25f894d0bf420b6f763341c2a0146a
Tanuja Shekokar <tanu.shekokar@gmail.com>
2025-01-20 15:58:42 +0530

    change connection str of db

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = async () => {
-    await mongoose.connect('mongodb://localhost:27017/inotebook');
+  //  await mongoose.connect('mongodb://localhost:27017/inotebook');
+  await mongoose.connect('mongodb+srv://Tanuja:Tanuja24@cluster0.gyby1.mongodb.net/');
     console.log("Connected to MongoDB");
 };
 
