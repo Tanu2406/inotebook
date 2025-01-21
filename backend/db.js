34c2a9cf8c008b7bcee3e9586ca2846be745cd9b
Tanuja Shekokar <tanu.shekokar@gmail.com>
2025-01-21 09:47:14 +0530

    change env file

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = async () => {
-  //  await mongoose.connect('mongodb://localhost:27017/inotebook');
-  await mongoose.connect('mongodb+srv://Tanuja:Tanuja24@cluster0.gyby1.mongodb.net/inotebook'
-  // {useNewUrlParser: true,
-   // useUnifiedTopology: true,}
+  await mongoose.connect('process.env.MONGO_URI',
+  {useNewUrlParser: true,
+   useUnifiedTopology: true,}
    );
     console.log("Connected to MongoDB");
 };
